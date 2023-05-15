498f2e72a7904c7810e4ebe6c3bc20f22459485c
aveillas <aurelien.veillas@assemblee-nationale.fr>
2023-05-15 14:30:55 +0200

    étape 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my_todo_list';
+
+  progress: number = 0;
+  intervalId: any;
+  tache1: string = "1st task to do";
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.progress+=1;
+      if (this.progress % 2 == 0 ) {
+        this.tache1 += ".";
+      }
+
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    this.intervalId.clear();
+  }
 }
